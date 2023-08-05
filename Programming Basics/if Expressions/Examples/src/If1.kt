8b68d3649d0b8ec3fc87e4a0615d74a9a24ca68f
Wonhui Roh <rwhui012@gmail.com>
2023-08-05 23:06:38 +0900

    1. 07 if 식

--- Programming Basics/if Expressions/Examples/src/If1.kt
+++ Programming Basics/if Expressions/Examples/src/If1.kt
@@ -1,10 +1,15 @@
-// IfExpressions/If2.kt
+// IfExpressions/If1.kt
 
 fun main() {
-  val x: Boolean = 1 >= 1 // 식을 따로 빼도 OK
-  if (x)
+  if (1 > 0)  // 결과값이 'Boolean'(true/false)인 '식'을 대입해도 OK
     println("It's true!")
+  if (10 < 11) {
+    println("10 < 11")
+    println("ten is less than eleven")
+  }
 }
 /* Output:
 It's true!
+10 < 11
+ten is less than eleven
 */
--- Programming Basics/if Expressions/Examples/src/If2.kt
+++ Programming Basics/if Expressions/Examples/src/If2.kt
@@ -1,7 +1,7 @@
 // IfExpressions/If2.kt
 
 fun main() {
-  val x: Boolean = 1 >= 1
+  val x: Boolean = 1 >= 1 // 식을 따로 빼도 OK
   if (x)
     println("It's true!")
 }
