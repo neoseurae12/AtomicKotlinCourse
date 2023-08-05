75b84c98f2b48b941904d320b5306ad47dbc1f87
Wonhui Roh <rwhui012@gmail.com>
2023-08-05 17:03:12 +0900

    1. 04 var & val

---
+++ var & val/Exercise 1/src/Task.kt
@@ -0,0 +1,10 @@
+// VarAndVal/Task1.kt
+//package varAndValExercise1
+package Exercise1
+
+fun main() {
+    var answer = 42
+    println(answer)
+    answer = 43
+    println(answer)
+}
---
+++ var & val/Exercise 5/src/Task.kt
@@ -0,0 +1,7 @@
+// VarAndVal/Task5.kt
+//package varAndValExercise5
+package Exercise5
+
+fun main() {
+  println("Hello, Kotlin!")
+}
