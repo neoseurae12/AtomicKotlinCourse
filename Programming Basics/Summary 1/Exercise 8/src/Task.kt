a899d5edbd880cf27a83f0ca78566c9ff2200ead
Wonhui Roh <rwhui012@gmail.com>
2023-08-07 16:38:46 +0900

    1. 15 1부 요약_Exercise 8 (정답)

--- Programming Basics/Summary 1/Exercise 8/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 8/src/Task.kt
@@ -2,14 +2,28 @@
 package summaryIExercise8
 
 fun reverseDecimal(number: Int): Int {
-  var result = ""
-  var decimal = number
+//  var result = ""
+//  var decimal = number
+//  while (decimal > 0) {
+//    result += (decimal % 10).toString()
+//    decimal /= 10
+//  }
+//
+//  return result.toInt()
+
+  // 위의 코드가 '오답'인 이유:
+    // input이 0일 경우에 에러가 발생하기 때문이다.
+
+  var result = 0
+  var decimal = number  // number는 val이기 때문에 number를 decimal로 따로 받아서 조작해야 한다.
+
   while (decimal > 0) {
-    result += (decimal % 10).toString()
+    result *= 10
+    result += decimal % 10
     decimal /= 10
   }
 
-  return result.toInt()
+  return result
 }
 
 fun main() {
