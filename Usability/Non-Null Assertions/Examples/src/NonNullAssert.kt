75ee9c9cba9918e0e8050072b645d197bea29395
Wonhui Roh <rwhui012@gmail.com>
2023-09-18 19:10:17 +0900

    3. 39 널 아님 단언 (이론)

---
+++ Usability/Non-Null Assertions/Examples/src/NonNullAssert.kt
@@ -0,0 +1,12 @@
+// NonNullAssertions/NonNullAssert.kt
+import atomictest.capture
+import atomictest.eq
+
+fun main() {
+  var x: String? = "abc"
+  x!! eq "abc"
+  x = null
+  capture {
+    val s: String = x!!
+  } eq "NullPointerException"
+}
